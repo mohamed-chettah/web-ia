1c95f38a0839bf782e78a4d9d77adee525899277
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-09-26 13:57:37 +0200

    wip frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import ListBien from './components/ListBien.vue'
+
+</script>
+
+<template>
+<ListBien />
+</template>
+
+<style scoped>
+
+</style>
